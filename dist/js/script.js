4b053df9132faec29e555c0484c487e74d8bbd64
Vladyslav <vladikzizu@gmail.com>
2020-06-28 15:39:43 +0200

    add cards

--- dist/js/script.js
+++ dist/js/script.js
@@ -98,16 +98,19 @@ document.addEventListener('DOMContentLoaded', () => {
           modal = document.querySelector('.modal');
 
     modalTrigger.forEach(btn =>{
-        btn.addEventListener('click', () => {
-            modal.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click',openModal);
     });
     
-   function closeModal() {
-    modal.style.display = 'none';
-    document.body.style.overflow = '';
-   }
+    function openModal(){
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
 
     modalClose.addEventListener('click', closeModal);
 
@@ -122,4 +125,84 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    /* const modalTimerId = setTimeout(openModal,2000); */
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll',showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll',showModalByScroll);
 });     
+
+
+// Add class cards
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector,...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.classes = classes;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 25;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            if(this.classes.length === 0) {
+                this.classes = 'menu__item';
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}/div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        12,
+        '.menu .container'
+    ).render();
